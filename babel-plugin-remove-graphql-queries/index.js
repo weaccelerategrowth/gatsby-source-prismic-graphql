33e38331715519990e8d059605480b9025fcb2f2
MarcMcIntosh <marcmcintosh1987@gmail.com>
2020-04-16 11:03:09 +0200

    re-built source files

--- babel-plugin-remove-graphql-queries/index.js
+++ babel-plugin-remove-graphql-queries/index.js
@@ -39,8 +39,7 @@ var isGlobalIdentifier = function isGlobalIdentifier(tag) {
 };
 
 function getGraphqlExpr(t, queryHash, source) {
-  return t.objectExpression([t.objectProperty(t.identifier('id'), t.stringLiteral(queryHash)), // t.objectProperty(t.identifier(`staticQueryData`), t.identifier(`data`)),
-  t.objectProperty(t.identifier('source'), t.stringLiteral(source)), t.objectMethod('method', t.identifier('toString'), [], t.blockStatement([t.returnStatement(t.memberExpression(t.identifier('this'), t.identifier('id')))]))]);
+  return t.objectExpression([t.objectProperty(t.identifier('id'), t.stringLiteral(queryHash)), t.objectProperty(t.identifier('source'), t.stringLiteral(source)), t.objectMethod('method', t.identifier('toString'), [], t.blockStatement([t.returnStatement(t.memberExpression(t.identifier('this'), t.identifier('id')))]))]);
 }
 
 var StringInterpolationNotAllowedError = /*#__PURE__*/function (_Error) {
@@ -264,9 +263,9 @@ function _default(_ref) {
               } // Add query
 
 
-              path2.replaceWith(getGraphqlExpr(t, this.queryHash, this.query)); // from gatsby
-              // path2.replaceWith(t.memberExpression(identifier, t.identifier(`data`)))
-              // Add import
+              path2.replaceWith(getGraphqlExpr(t, this.queryHash, this.query)); // Add siteMetaData query
+
+              path2.replaceWith(t.memberExpression(identifier, t.identifier("data"))); // Add import
 
               var importDefaultSpecifier = t.importDefaultSpecifier(identifier);
               var importDeclaration = t.importDeclaration([importDefaultSpecifier], t.stringLiteral(filename ? nodePath.relative(nodePath.parse(filename).dir, resultPath) : shortResultPath));
