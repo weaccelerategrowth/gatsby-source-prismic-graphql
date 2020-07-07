a2bf3aca5f83e3135521a150e55a1409aa4455fc
MarcMcIntosh <marcmcintosh1987@gmail.com>
2020-07-07 13:26:37 +0200

    fix: pagination example

--- examples/pagination/src/components/image.js
+++ examples/pagination/src/components/image.js
@@ -13,20 +13,24 @@ import Img from 'gatsby-image';
  * - `StaticQuery`: https://gatsby.dev/staticquery
  */
 
-const Image = () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-          childImageSharp {
-            fluid(maxWidth: 300) {
-              ...GatsbyImageSharpFluid
-            }
+const Image = () => {
+  const query = graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
           }
         }
       }
-    `}
-    render={data => <Img fluid={data.placeholderImage.childImageSharp.fluid} />}
-  />
-);
+    }
+  `;
+  return (
+    <StaticQuery
+      query={`${query}`}
+      render={data => <Img fluid={data.placeholderImage.childImageSharp.fluid} />}
+    />
+  );
+};
+
 export default Image;
--- examples/pagination/src/components/layout.js
+++ examples/pagination/src/components/layout.js
@@ -12,17 +12,19 @@ import { StaticQuery, graphql } from 'gatsby';
 import Header from './header';
 import './layout.css';
 
+const query = graphql`
+  query SiteTitleQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
+
 const Layout = ({ children }) => (
   <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
+    query={`${query}`}
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
