214baef7afe1d3cd44b619ed12a9f4fab1d31285
Matthew T. Gilbride <mgilbride@chariotsolutions.com>
2020-07-23 18:49:10 -0400

    feat(withpreview): tweak the type of withPreview.tsx
    Tweak the type in the lightest possible way, without changing any source.

--- packages/gatsby-source-prismic-graphql/src/components/withPreview.tsx
+++ packages/gatsby-source-prismic-graphql/src/components/withPreview.tsx
@@ -1,32 +1,25 @@
 import React from 'react';
 import { WrapPage } from './WrapPage';
-import { StaticQueryProps } from 'gatsby';
 
-export type StaticQueryRender<T> = Required<StaticQueryProps<T>>['render'];
-
-export const withPreview = <T extends any = any>(
-  render: StaticQueryRender<T>,
+export const withPreview = <Data extends any = any>(
+  render: (data: Data) => JSX.Element,
   query: any,
   fragments: any = []
-): StaticQueryRender<T> => {
+): ((data: Data) => JSX.Element) | null => {
   if (typeof window === 'undefined') {
     return render;
   }
 
-  // shouldn't the type of the render argument be `render?: ...` if we are going to handle it not being defined?
-  // this is the case that someone would call `withPreview()` with no arguments...why not just throw an error?
   if (!render) {
-    return (null as unknown) as StaticQueryRender<T>;
+    return null;
   }
 
-  // this is interesting (a.k.a. concerning) - has to be case to JSX.Element for some reason
-  const RenderComponent = ({ data }: any) => render(data) as JSX.Element;
-  // it would be nice to narrow down the `any` for the query argument, but `typeof graphql` doesn't work...also interesting
+  const RenderComponent = ({ data }: any) => render(data);
   const rootQuery = `${query.source}${fragments
     .map((fragment: any) => (fragment && fragment.source ? fragment.source : ''))
     .join(' ')}`;
 
-  return (data: T) => (
+  return (data: any) => (
     <WrapPage
       data={data}
       pageContext={{ rootQuery }}
